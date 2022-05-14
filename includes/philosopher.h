b9baca7b782e06c3a510cf9bd5d02b520acf76b5
Soufiane Amajat <samajat@e2r4p1.1337.ma>
2022-05-14 23:24:36 +0100

    building actions

--- includes/philosopher.h
+++ includes/philosopher.h
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 21:07:04 by samajat           #+#    #+#             */
-/*   Updated: 2022/05/14 00:53:18 by samajat          ###   ########.fr       */
+/*   Updated: 2022/05/14 23:16:20 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,21 +45,30 @@ typedef struct s_fork
 {
     int             fork_index;
     int             fork_available;
+    int             fork_token;
     pthread_mutex_t fork_mutex;
 }   t_fork;
 
 typedef struct s_philosopher
 {
-    int identity;
-    int status;
-    int right_fork_available;
-    int left_fork_available;
+    int     identity;
+    int     status;
+    t_fork  right_fork_available;
+    t_fork  left_fork_available;
 }   t_philosopher;
 
+
+//Init
+void    initialize_user_input (t_input  *uinput, char   **argv);
+
 //Philosopher situations
-int do_sleeping ();
+int do_sleeping (t_input *input, t_philosopher *philosopher);
 int do_eating ();
 int do_thinking ();
+
+//ft_time
+long    get_passed_time_in_milli(long program_launch_time, long accual_time);
+long    get_actual_time_in_milliseconds();
 
 //utils
 int	ft_atoi(const char *nptr);
